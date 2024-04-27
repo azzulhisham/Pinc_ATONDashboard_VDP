b9344bc73af4fac3c2f77e529ba9f5f7e15a1c7d
zulhisham <zultan@pinc.my>
2024-04-28 01:33:48 +0800

    added chart for ATON and Lantern for last 24 hours

--- py_services/Comm_Socket.py
+++ py_services/Comm_Socket.py
@@ -173,6 +173,24 @@ async def handler(websocket, path):
                 await websocket.send(json.dumps(data))  
 
 
+            if msg[0] == 'getallatonvoltdata':
+                atons = PyCH.get_aton_voltdata(msg[1])
+
+                for i in atons:
+                    data = {
+                        'payload': 'getallatonvoltdata',
+                        'mmsi': msg[1]
+                    }
+
+                    data.update(i)
+                    await websocket.send(json.dumps(data)) 
+
+                data = {
+                    'payload': 'getallatonvoltdata_done'
+                } 
+
+                await websocket.send(json.dumps(data))  
+
 
             if msg[0] == 'getallatonmsg':
                 atons = PyCH.get_all_aton_msg()
