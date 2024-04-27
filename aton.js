eb53eb0cb109be0727dea71e06494c37ba3c1d1a
zulhisham <zultan@pinc.my>
2024-04-27 12:41:25 +0800

    Added chart features for selected ATON to show last 24 hours battery condition

--- aton.js
+++ aton.js
@@ -356,12 +356,15 @@ closeChartButton.addEventListener('click', () => {
 
 // Dialog Report Handling
 showReport.addEventListener('click', () => {
+    data_table.innerHTML = ''
+    build_tabulator_table()
     dialogShowReport.classList.remove('hidden');
 });
 
 closeReportButton.addEventListener('click', () => {
     dialogShowReport.classList.add('hidden');
 });
+
 
 ///////////////////
 //     mapbox
@@ -821,6 +824,12 @@ async function radicalMenuClick(e) {
         if (elemid_text[0] == 'sp12'){
             vessel_info_panel(elemid_text[1])
             vesselInfo.classList.add('openwide')
+        }
+
+        if (elemid_text[0] == 'sp6'){
+            data_table.innerHTML = ''
+            build_chart()
+            dialogShowReport.classList.remove('hidden');
         }
     }
 }
@@ -1407,6 +1416,7 @@ function clearHeartbeat2() {
 
 // setInterval(updateDashboard, 30000);
 
+
 function build_tabulator_table() {
     //Build Tabulator
     var table = new Tabulator("#data-table", {
@@ -1468,4 +1478,85 @@ function build_tabulator_table() {
     // document.getElementById("download-html").addEventListener("click", function(){
     //     table.download("html", "data.html", {style:true});
     // });
+}
+
+
+function build_chart(){
+    Highcharts.chart('data-table', {
+
+        title: {
+            text: 'U.S Solar Employment Growth',
+            align: 'left'
+        },
+    
+        subtitle: {
+            text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
+            align: 'left'
+        },
+    
+        yAxis: {
+            title: {
+                text: 'Number of Employees'
+            }
+        },
+    
+        xAxis: {
+            accessibility: {
+                rangeDescription: 'Range: 2010 to 2020'
+            }
+        },
+    
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+    
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 2010
+            }
+        },
+    
+        series: [{
+            name: 'Installation & Developers',
+            data: [43934, 48656, 65165, 81827, 112143, 142383,
+                171533, 165174, 155157, 161454, 154610]
+        }, {
+            name: 'Manufacturing',
+            data: [24916, 37941, 29742, 29851, 32490, 30282,
+                38121, 36885, 33726, 34243, 31050]
+        }, {
+            name: 'Sales & Distribution',
+            data: [11744, 30000, 16005, 19771, 20185, 24377,
+                32147, 30912, 29243, 29213, 25663]
+        }, {
+            name: 'Operations & Maintenance',
+            data: [null, null, null, null, null, null, null,
+                null, 11164, 11218, 10077]
+        }, {
+            name: 'Other',
+            data: [21908, 5548, 8105, 11248, 8989, 11816, 18274,
+                17300, 13053, 11906, 10073]
+        }],
+    
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    
+    });
 }
